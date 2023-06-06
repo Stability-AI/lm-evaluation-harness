27994869e0f1446353884e0ea234c6b134a08169
fujiki <fujiki.nakamura@stability.ai>
2023-06-06 14:23:11 +0900

    add stablelm-jp-instruct-1b_1.3.0

---
+++ models/stablelm-jp-instruct-1b_1.3.0/harness.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MODEL_ARGS="pretrained=/PATH/TO/stablelm-jp-instruct-1b_1.3.0/checkpoint-5343/,tokenizer=/PATH/TO/nai-hf-tokenizer/,use_fast=False"
+TASK="jsquad-1.1-0.3,jcommonsenseqa-1.1-0.3,jnli-1.1-0.3,marc_ja-1.1-0.3"
+python main.py \
+    --model hf-causal \
+    --model_args $MODEL_ARGS \
+    --tasks $TASK \
+    --num_fewshot "2,3,3,3" \
+    --device "cuda" \
+    --output_path "models/stablelm-jp-instruct-1b_1.3.0/result.json"
